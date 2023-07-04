668fc189c26e971af985520945f9be0cb4c3c3c7
Rohit Kumar <rohit96682@gmail.com>
2023-07-04 23:27:01 +0530

    reactive form

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -11,12 +11,23 @@ export class ReactiveFormComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  profileForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
   });
 
   onSubmit() {
-    console.log(this.profileForm.value);
+    console.log(this.loginForm.value);
+  }
+
+  get emailValidator() {
+    return this.loginForm.get('email');
+  }
+
+  get passwordValidator() {
+    return this.loginForm.get('password');
   }
 }
